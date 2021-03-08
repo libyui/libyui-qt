051de2536fead94bcb56b6b18e77ea08cf8f386d
José Iván López González <jlopez@suse.com>
2021-03-08 14:56:49 +0000

    Remove all files

--- .dir-locals.el
+++
@@ -1,3 +0,0 @@
-; https://www.gnu.org/software/emacs/manual/html_node/emacs/Directory-Variables.html#Directory-Variables
-((c++-mode . ((indent-tabs-mode . nil)
-              (c-basic-offset   . 4) )))
